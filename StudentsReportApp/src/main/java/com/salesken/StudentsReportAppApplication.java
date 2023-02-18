1663c3daf9ca40fe4ef8d46d7a8fce3c26be9d73
vinayak narlawar <narlawarvinayak@gmail.com>
2023-02-18 16:02:46 +0530

    database

---
+++ StudentsReportApp/src/main/java/com/salesken/StudentsReportAppApplication.java
@@ -0,0 +1,13 @@
+package com.salesken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsReportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsReportAppApplication.class, args);
+	}
+
+}
